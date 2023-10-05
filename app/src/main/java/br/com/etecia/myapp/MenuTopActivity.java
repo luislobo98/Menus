a15e68e5b3a6f55889b970e5ce98d53cb6613f65
luislobo98 <luis.santos629@etec.sp.gov.br>
2023-10-05 20:09:13 -0300

    Menus

---
+++ app/src/main/java/br/com/etecia/myapp/MenuTopActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuTopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_menu_top);
+    }
+}
